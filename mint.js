ee6be692ca2c3c50eba257399402819b17ceb349
Luis <luis.poveda9321@gmail.com>
2022-01-10 21:52:01 +0100

    mint

--- mint.js
+++ mint.js
@@ -1,53 +1,53 @@
 require("dotenv").config()
-const API_URL = process.env.API_URL;
-const PUBLIC_KEY = process.env.PUBLIC_KEY;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const API_URL = process.env.API_URL
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
 
 const contract = require("./build/contracts/MyToken.json")
-console.log(JSON.stringify(contract.abi))
 const contractAddress = "0x4661FF6651977F479F39Ff59D95f0B84465665b9"
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
 
 async function mintNFT(tokenURI) {
-    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest") //get latest nonce
 
   //the transaction
-    const tx = {
-      'from': PUBLIC_KEY,
-      'to': contractAddress,
-      'nonce': nonce,
-      'gas': 500000,
-      'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
-    }
-  
-    const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
-    signPromise
-      .then((signedTx) => {
-        web3.eth.sendSignedTransaction(
-          signedTx.rawTransaction,
-          function (err, hash) {
-            if (!err) {
-              console.log(
-                "The hash of your transaction is: ",
-                hash,
-                "\nCheck Alchemy's Mempool to view the status of your transaction!"
-              )
-            } else {
-              console.log(
-                "Something went wrong when submitting your transaction:",
-                err
-              )
-            }
-          }
-        )
-      })
-      .catch((err) => {
-        console.log(" Promise failed:", err)
-      })
+  const tx = {
+    from: PUBLIC_KEY,
+    to: contractAddress,
+    nonce: nonce,
+    gas: 500000,
+    data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
   }
 
-  mintNFT(
-    "https://gateway.pinata.cloud/ipfs/QmX6T942BdeDwLaTjfnAQKzDTKvQk4fWjhMEyVY453Yh3M"
-  )
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
+  signPromise
+    .then((signedTx) => {
+      web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of your transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of your transaction!"
+            )
+          } else {
+            console.log(
+              "Something went wrong when submitting your transaction:",
+              err
+            )
+          }
+        }
+      )
+    })
+    .catch((err) => {
+      console.log("Promise failed:", err)
+    })
+}
+
+mintNFT(
+  "https://gateway.pinata.cloud/ipfs/QmX6T942BdeDwLaTjfnAQKzDTKvQk4fWjhMEyVY453Yh3M"
+)
